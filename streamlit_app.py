04f11d478b7ce6c5ffed9ed357cf66c95931188a
abdusalam7474 <mideola824@gmail.com>
2024-05-15 22:11:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,10 @@ def main():
         st.image("pneumo-false.jpeg", caption="X-ray image of a healthy lung")
 
     # Prediction tab content
+    st.write("Upload an Image to test for pneumonia and click.")
+    st.write("Accepted file formats: jpg, png")
     with tab2:
-        uploaded_file = st.file_uploader("Upload Chest X-ray Image", type="jpg,png")
+        uploaded_file = st.file_uploader("Upload Chest X-ray Image", type=["jpg", "png"])
         if uploaded_file is not None:
             image = load_image(uploaded_file)
             st.image(image, caption="Uploaded Image")
